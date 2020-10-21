d4187a697abbf6f2789c90e245b54e0b2ba84540
lavreshka <lavreshka23@gmail.com>
2020-10-21 17:34:41 +0300

    Added choice-routing

--- mywebsite/polls/urls.py
+++ mywebsite/polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index,name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.index,name='index_url'),
+    path('<str:question_text>/answers/', views.detail, name='detail_url'),
+    path('<str:question_text>/results/', views.results, name='results_url'),
+    path('<str:question_text>/vote/', views.vote, name='vote_url'),
+    path('<str:question_text>/answers/<str:choice_text>/votes_amount/', views.votes_amount, name='votes_amount_url')
 ]
--- mywebsite/polls/views.py
+++ mywebsite/polls/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def redirect_polls(request):
     return redirect('index', permanent=True)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_text):
+    question = get_object_or_404(Question, question_text=question_text)
+    return render(request, 'polls/detail.html', context={'question':question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, question_text):
+    response = "You're looking at the results of question '%s'"
+    return HttpResponse(response % question_text)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, question_text):
+    return HttpResponse("You're voting on question '%s'" % question_text)
+
+def votes_amount(request, question_text, choice_text):
+    question = Question.objects.get(question_text=question_text)
+    votes_amount = question.choice_set.get(choice_text=choice_text).votes
+    return render(request, 'polls/votes_amount.html', context={'question':question, 'votes_amount':votes_amount})
