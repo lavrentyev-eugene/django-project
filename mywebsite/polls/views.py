14b7724802bef3e9a6e567cd2f16da28097af573
lavreshka <lavreshka23@gmail.com>
2020-11-02 16:03:27 +0300

    Added few tests

--- mywebsite/polls/views.py
+++ mywebsite/polls/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404, reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
 def redirect_polls(request):
-    return redirect('index', permanent=True)
+    return redirect('polls:index_url', permanent=True)
 
 
 class IndexView(generic.ListView):
@@ -13,14 +14,25 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-    def get_query_set(self):
-        # Returns the last five published questions
-        return Question.objects.order_by('-pub_date')[:5]
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set
+        to be published in future).
+        """
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
